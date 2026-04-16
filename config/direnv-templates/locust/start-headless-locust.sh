957547472032ea560d92249308565b92921f29bc
Zeno Belli <xeno@os76.xyz>
2026-04-16 15:38:44 +0200

    fix: locust direnv conf file

--- config/direnv-templates/locust/start-headless-locust.sh
+++ config/direnv-templates/locust/start-headless-locust.sh
@@ -1,4 +1,3 @@
-❯ cat start-headless-locust.sh
 #!/usr/bin/env bash
 
 locust -f locustfile.py --headless -u 1 -r 1 -H https://dest.host.address
--- config/direnv-templates/locust/start-locust.sh
+++ config/direnv-templates/locust/start-locust.sh
@@ -1,4 +1,3 @@
-❯ cat start-headless-locust.sh
 #!/usr/bin/env bash
 
 locust -f locustfile.py -H https://dest.host.address
